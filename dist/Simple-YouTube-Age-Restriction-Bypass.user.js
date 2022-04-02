33e62eb0dd681d91b0c7374af7c951cd11069589
NiborH68 <64282476+NiborH68@users.noreply.github.com>
2022-04-02 15:23:08 +0200

    Update Simple-YouTube-Age-Restriction-Bypass.user.js
    version number

--- dist/Simple-YouTube-Age-Restriction-Bypass.user.js
+++ dist/Simple-YouTube-Age-Restriction-Bypass.user.js
@@ -5,7 +5,7 @@
 // @description:de  Schaue YouTube Videos mit Altersbeschränkungen ohne Anmeldung und ohne dein Alter zu bestätigen :)
 // @description:fr  Regardez des vidéos YouTube avec des restrictions d'âge sans vous inscrire et sans confirmer votre âge :)
 // @description:it  Guarda i video con restrizioni di età su YouTube senza login e senza verifica dell'età :)
-// @version         2.4.6
+// @version         2.4.7
 // @updateURL       https://github.com/NiborH68/Simple-YouTube-Age-and-Workspace-Restriction-Bypass/raw/main/dist/Simple-YouTube-Age-Restriction-Bypass.user.js
 // @downloadURL     https://github.com/NiborH68/Simple-YouTube-Age-and-Workspace-Restriction-Bypass/raw/main/dist/Simple-YouTube-Age-Restriction-Bypass.user.js
 // @author          Zerody (https://github.com/zerodytrash)
